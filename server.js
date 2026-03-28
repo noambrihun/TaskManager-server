61fe66043b42dd0323307584996dc25009fdbbfd
noambrihun <noambrihun1@gmail.com>
2026-03-28 22:33:48 +0300

    fix: correct MONGO_URI env variable

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use("/api", tasksRoutes)
 
 
-mongoose.connect(process.env.Mongo_URI)
+mongoose.connect(process.env.MONGO_URI)
 .then(()=> console.log("MongoDB connected"))
 .catch(err => console.log(err))
 
